0c18cddd7d480c514ee6b6e4627b408fdd55b1e0
deathwatchgaming <deathwatchgaming@gmail.com>
2024-09-29 21:20:10 -0700

    Still continue start creating / adding 4 of vehicle type(s)
    Start creating / adding 4 of vehicle type(s) and prepping for such transitions to eventually then also modify all docs to reflect the changes
    
    so current 8 vehicles will become 32 vehicles
    
    Current completed / added:
    PoliceCruiser
    Ambulance
    Taxi
    Van
    
    Ie: current Ambulance will become:
    
    Ambulance01
    Ambulance02
    Ambulance03
    Ambulance04
    
    Ie: current PoliceCruiser will become:
    
    PoliceCruiser01
    PoliceCruiser02
    PoliceCruiser03
    PoliceCruiser04
    
    Ie: current Taxi will become:
    
    Taxi01
    Taxi02
    Taxi03
    Taxi04
    
    Ie: current Van will become:
    
    Van01
    Van02
    Van03
    Van04

---
+++ Extras/VehicleController-Only-Usage/Vehicle-Speedometer/Assets/VehiclesControl/Van/Van03/Scripts/Van03Speedometer.cs
@@ -0,0 +1,148 @@
+/*
+ * File: Van 03 Speedometer
+ * Name: Van03Speedometer.cs
+ * Author: DeathwatchGaming
+ * License: MIT
+ */
+
+// using
+using TMPro;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+// namespace VehiclesControl
+namespace VehiclesControl
+{
+    // public enum Van03SpeedUnit
+	public enum Van03SpeedUnit
+	{
+		mph,
+		kmh	
+
+	} // close public enum Van03SpeedUnit
+
+	// public class Van03Speedometer 
+	public class Van03Speedometer : MonoBehaviour
+	{
+		// Speedometer
+		[Header("Speedometer")]
+
+			[Tooltip("The speedometer needle")]
+			// GameObject _needle
+			[SerializeField] private GameObject _needle;
+
+			[Tooltip("The start position")]
+			// float _startPosition
+			[SerializeField] private float _startPosition = 220f;
+
+			[Tooltip("The end position")]
+			// float _endPosition
+			[SerializeField] private float _endPosition = -41f;
+
+			[Tooltip("The desired position")]
+			// float _desiredPosition
+			[SerializeField] private float _desiredPosition;
+
+			[Tooltip("The vehicle speed")]
+			// float _vehicleSpeed
+			[SerializeField] private float _vehicleSpeed = 0f;
+
+ 		// Speed Text       
+		[Header("Speed Text")]
+
+			[Tooltip("The decimal places")]
+			// int _decimalPlaces 
+			[SerializeField] private int _decimalPlaces = 0;
+
+			[Tooltip("The previous text string")]
+			//  string _previousText		
+			[SerializeField] private string _previousText;
+
+			[Tooltip("The speed measurement unit")]
+			// Van03SpeedUnit _speedUnit	
+			[SerializeField] private Van03SpeedUnit _speedUnit;
+
+			[Tooltip("The interface speed amount text")]
+			// TMP_Text speedText
+			[SerializeField] private TMP_Text _speedText;
+
+		// float _currentSpeed
+		float _currentSpeed;
+
+		// Van03Controller _van03Script
+		private Van03Controller _van03Script;
+
+		// Rigidbody _rigidbody
+		private Rigidbody _rigidbody;
+
+		// private void Start
+		private void Start()
+		{
+			// _van03Script is GetComponent Van03Controller
+			_van03Script = GetComponent<Van03Controller>();
+
+			// _rigidbody
+			_rigidbody = GetComponent<Rigidbody>();	
+
+		} // close private void Start
+
+		// private void Update
+		private void Update()
+		{
+			// if _speedUnit equals Van03SpeedUnit.mph
+			if (_speedUnit == Van03SpeedUnit.mph)
+			{
+				// 2.23694 is the constant to convert a value from m/s to mph
+
+				// _currentSpeed
+				_currentSpeed = (float)Math.Round(_rigidbody.velocity.magnitude * 2.23694f, _decimalPlaces);
+
+				// _speedText.text
+				_speedText.text = _previousText + _currentSpeed.ToString() + " mph";
+
+			} // close if _speedUnit equals Van03SpeedUnit.mph
+
+			// else 
+			else 
+			{
+				// 3.6 is the constant to convert a value from m/s to km/h
+
+				// _currentSpeed
+				_currentSpeed = (float)Math.Round(_rigidbody.velocity.magnitude * 3.6f, _decimalPlaces);
+
+				// _speedText.text
+				_speedText.text = _previousText + _currentSpeed.ToString() + " km/h";
+
+			} // close else
+
+		} // close private void Update
+
+		// private void FixedUpdate
+		private void FixedUpdate()
+		{
+			// _vehicleSpeed
+			_vehicleSpeed = _currentSpeed;
+
+			// UpdateSpeedoNeedle
+			UpdateSpeedoNeedle();
+
+		} // close private void FixedUpdate
+
+		// private void UpdateSpeedoNeedle
+		private void UpdateSpeedoNeedle()
+		{
+			// _desiredPosition
+			_desiredPosition = _startPosition - _endPosition;
+
+			// float _temp 
+			float _temp = _vehicleSpeed / 180;
+
+			// _needle.transform.eulerAngles
+			_needle.transform.eulerAngles = new Vector3(0, 0, (_startPosition -_temp * _desiredPosition));
+
+		} // close private void UpdateSpeedoNeedle				
+
+	} // close public class Van03Speedometer
+
+} // close namespace VehiclesControl
