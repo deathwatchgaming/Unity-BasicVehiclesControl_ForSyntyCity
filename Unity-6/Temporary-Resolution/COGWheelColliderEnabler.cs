e99166cc899725f8e1c7979d6abd3bc9d46a4ea9
deathwatchgaming <deathwatchgaming@gmail.com>
2024-10-25 03:30:30 -0700

    U-6 Temporary Resolution
    NOTE: There is currently a bug in Unity 6 that affects wheel colliders and center of gravity offset. This bug has been reported and is listed as a bug and is currently being worked on for an official fix. Until such we can provide a temporary resolution file to add to your vehicle to sort this out.

---
+++ Unity-6/Temporary-Resolution/COGWheelColliderEnabler.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+namespace TemporarilyResolve
+{
+	public class COGWheelColliderEnabler : MonoBehaviour
+	{
+
+		[Header("Wheel Colliders")]
+
+			[Tooltip("Add just one of any of the wheel colliders")]
+			[SerializeField] private WheelCollider _wheelCollider;
+
+		[Header("COG Offset")]
+
+			[Tooltip("Zero out the center of gravity offset in your controller first as this replaces such for now")]
+		    [SerializeField] private float _centreOfGravityOffset = -1f;
+
+		Rigidbody _rigidbody;
+
+		private void Awake()
+		{
+		    _rigidbody = GetComponent<Rigidbody>();
+
+		    _wheelCollider.enabled = false;
+
+		    float duration = 0.000001f;
+
+		    StartCoroutine(EnableWheelCollider(duration));
+		}
+
+		private IEnumerator EnableWheelCollider(float duration)
+		{
+		    yield return new WaitForSeconds(duration);
+
+		    // Adjust center of mass vertically, to help prevent the car from rolling
+		    _rigidbody.centerOfMass += Vector3.up * _centreOfGravityOffset;
+
+		    _wheelCollider.enabled = true;
+
+		}
+	}	
+}
